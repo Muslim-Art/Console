439163945cdd98950594d36380c84c40ae0bd5c0
Shwed <call_me_tatarin.com>
2023-08-03 15:34:39 +0000

    E003_Random

--- E003_Sum/Program.cs
+++ E003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int number_a = 3;
-int number_b = 5;
+﻿int number_a = new Random().Next(1,10); //1 2 3 4 ... 9
+Console.WriteLine(number_a);
+int number_b = new Random().Next(1,10);
+Console.WriteLine(number_b);
 int result = number_b + number_a;
 Console.WriteLine(result);
 
